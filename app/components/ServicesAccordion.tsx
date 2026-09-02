a8651c74b8b45198cfa013502a33a01fcebc926b
Shrikant <shrikant@example.com>
2026-09-02 14:40:37 +0530

    added /public to images

--- app/components/ServicesAccordion.tsx
+++ app/components/ServicesAccordion.tsx
@@ -17,7 +17,7 @@ const serviceData: ServiceItem[] = [
     title: "Brand Building, Design & Management",
     collapsedColor: "#912A02", // burnt orange
     expandedColor: "#912A02", // lightened burnt orange (~25-30%)
-    image: "/images/service/tab-1-image.png",
+    image: "public/images/service/tab-1-image.png",
     subServices: [
       "Brand Strategy",
       "Visual Identity & Packaging",
@@ -30,7 +30,7 @@ const serviceData: ServiceItem[] = [
     title: "Social Media & Creative Marketing",
     collapsedColor: "#052446", // deep purple
     expandedColor: "#052446", // lightened mauve
-    image: "/images/service/tab-2-image.png",
+    image: "public/images/service/tab-2-image.png",
     subServices: [
       "Social Media Strategy",
       "Content Creation & Curation",
@@ -43,7 +43,7 @@ const serviceData: ServiceItem[] = [
     title: "Website Design & Build",
     collapsedColor: "#4E0000", // olive
     expandedColor: "#4E0000", // lightened olive
-    image: "/images/service/tab-3-image.png",
+    image: "public/images/service/tab-3-image.png",
     subServices: [
       "UI/UX Design",
       "Responsive Web Development",
